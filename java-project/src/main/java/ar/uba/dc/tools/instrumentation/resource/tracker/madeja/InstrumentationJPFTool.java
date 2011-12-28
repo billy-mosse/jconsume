2ffb648ebe26ca544e86ae4cbc93f4a0f4140c05
Diego Garbervetsky <diego.garbervetsky@gmail.com>
2011-12-28 11:00:30 -0300

    new file:   src/main/java/ar/uba/dc/tools/instrumentation/resource/tracker/madeja/InstrumentationJPFTool.java 	new file:   src/main/java/ar/uba/dc/tools/instrumentation/resource/tracker/madeja/InstrumenterForJPF.java 	new file:   ../jpfHelper/src/monitor 	modified:   .classpath 	modified:   .project 	modified:   bin/barvinok.sh 	modified:   bin/run.sh 	typechange: config.properties 	modified:   src/main/examples/javazoom/jl/decoder/JavaLayerUtils.java 	modified:   src/main/examples/javazoom/jl/decoder/SynthesisFilter.java 	modified:   src/main/examples/javazoom/jl/player/JavaSoundAudioDevice.java 	modified:   src/main/examples/javazoom/jl/player/Player.java

---
+++ java-project/src/main/java/ar/uba/dc/tools/instrumentation/resource/tracker/madeja/InstrumentationJPFTool.java
@@ -0,0 +1,132 @@
+package ar.uba.dc.tools.instrumentation.resource.tracker.madeja;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang.ClassUtils;
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import soot.Scene;
+import soot.SootClass;
+import soot.jimple.toolkits.callgraph.EdgePredicate;
+import soot.jimple.toolkits.callgraph.ReachableMethods;
+import ar.uba.dc.analysis.common.RepositoryLoaderTransformer;
+import ar.uba.dc.analysis.common.SummaryRepository;
+import ar.uba.dc.analysis.escape.EscapeSummary;
+import ar.uba.dc.analysis.escape.runner.EscapeSceneTransformer;
+import ar.uba.dc.analysis.escape.summary.repository.RAMSummaryRepository;
+import ar.uba.dc.analysis.memory.impl.runner.PhaseInitializer;
+import ar.uba.dc.config.Context;
+import ar.uba.dc.config.ContextFactory;
+import ar.uba.dc.soot.SootUtils;
+import ar.uba.dc.tools.instrumentation.resource.tracker.runtime.ResourceTrackerListener;
+import ar.uba.dc.util.Timer;
+
+@SuppressWarnings("unused")
+public class InstrumentationJPFTool {
+	
+	private static Log log = LogFactory.getLog(InstrumentationJPFTool.class);
+	
+	public static void main(final String[] args) throws IOException {
+		String propertiesFile = null;
+		boolean runEscapeBefore = true;
+		
+		if (args.length >= 2) {
+			if (StringUtils.isNotBlank(args[1])) {
+				propertiesFile = args[1];
+			}
+		}
+		
+		if (args.length >= 3) {
+			if (StringUtils.isNotBlank(args[2])) {
+				runEscapeBefore = Boolean.valueOf(args[2].trim().toLowerCase());
+			}
+		}
+
+		log.info("Instrumentation for [" + args[0] + "] using [" + StringUtils.defaultString(propertiesFile, "null") + "] as configuration");
+		
+		final Context context = ContextFactory.getContext(propertiesFile, false);
+
+		EdgePredicate predicate = context.getFactory().getEdgePredicate();
+		if (predicate != null) {
+			ReachableMethods.setEdgePredicate(predicate);
+		}
+		
+		String[] opts = { 
+	//		"-w",
+			"-app",
+			"-soot-classpath", context.getString(Context.APPLICATION_CLASSPATH),
+			"-f", context.getString(Context.INSTRUMENTATION_OUTPUT_FORMAT),
+			//"-f", "j",
+			
+			"-d", context.getString(Context.INSTRUMENTATION_OUTPUT_FOLDER),
+			"-src-prec","class",
+			"-keep-line-number", 
+			"-keep-bytecode-offset",
+			"-i", "monitor",
+			"-x", "gnu", 
+			"-x", "spec.io",
+			"-x", "java.lang.StringBuffer",
+			"-p", "jb",	"use-original-names:true", 
+			
+			//Agregadas
+			"-p","jb.a","enabled:false",
+			"-p","jb.uce","enabled:false",
+			"-p","jb.ne","enabled:false",
+			"-p", "jb.dae", "enabled:false",
+			"-p", "jb.ule", "enabled:false",
+			"-p", "jb.ulp", "enabled:true",
+			
+			
+			"-p", "jj",	"use-original-names:true", 
+			//"-p", "jj.dae", "enabled:false",
+			//"-p", "jj.ule", "enabled:false",
+			"-p", "jj.ulp", "enabled:true",
+			"-p", "cg.spark", "enabled:true",
+			"-allow-phantom-refs",
+			"-main-class", args[0], 
+			args[0]
+		}; 
+		
+		//Escape analysis
+		if (runEscapeBefore) {
+			PhaseInitializer initializer = context.getFactory().getEscapePhaseInitializer();
+			initializer.initialize(context, args[0]);
+		} 
+		
+		
+		LifeTimeSummaryRepository repository = new LifeTimeSummaryRepository();
+		/**
+		 * LifeTimeOracle Analysis
+		 * Genera un resumen de todos los metodos a instrumentar en funcion del CG. Al finalizar hace un write de las propiedas
+		 * usada luego por el Runtime para el binding de particiones
+		 */
+		//LifeTimeOracleSceneTransformer transformer = LifeTimeOracleSceneTransformer.v(context, args[0], repository); //Configurar el repo
+		//transformer.setRepository(repository);
+		//SootUtils.insertTransformer("wjtp", "wjtp.la", transformer);	
+		
+		Scene.v().addBasicClass(InstrumenterForJPF.LISTENER_CLASS, SootClass.SIGNATURES);
+		SootUtils.insertTransformer("jtp", "jtp.instrumentation", new InstrumenterForJPF(args[0], repository));	
+		
+		
+		Timer analysisTimer = new Timer();
+		analysisTimer.start();
+		
+		soot.Main.main(opts);
+		
+		//String relativePath = ClassUtils.getPackageCanonicalName(ResourceTrackerListener.class).replaceAll("\\.", "/") + "/tracker.properties";
+		//String srcDir = context.getString(Context.TOOL_CLASSPATH) + "/tracker.properties";
+		
+		
+		String dstDir = context.getString(Context.INSTRUMENTATION_OUTPUT_FOLDER) + "/" + LifeTimeOracleAnalysis.OUTPUT_RELATIVE_PATH;		
+		FileUtils.copyFile(new File(LifeTimeOracleAnalysis.OUTPUT_RELATIVE_PATH), new File(dstDir));
+		
+		analysisTimer.stop();
+		
+		log.info("Finish. Took " + analysisTimer.getElapsedTime() + " (" + analysisTimer.getElapsedSeconds() + " seconds)");
+	}	
+	
+}
---
+++ java-project/src/main/java/ar/uba/dc/tools/instrumentation/resource/tracker/madeja/InstrumenterForJPF.java
@@ -0,0 +1,364 @@
+package ar.uba.dc.tools.instrumentation.resource.tracker.madeja;
+
+import monitor.BooleanTracker;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import soot.Body;
+import soot.BodyTransformer;
+import soot.Scene;
+import soot.SootMethod;
+import soot.Unit;
+import soot.Value;
+import soot.VoidType;
+import soot.jimple.AnyNewExpr;
+import soot.jimple.AssignStmt;
+import soot.jimple.CaughtExceptionRef;
+import soot.jimple.IdentityStmt;
+import soot.jimple.IntConstant;
+import soot.jimple.InvokeExpr;
+import soot.jimple.Jimple;
+import soot.jimple.NewArrayExpr;
+import soot.jimple.NewExpr;
+import soot.jimple.NewMultiArrayExpr;
+import soot.jimple.ReturnStmt;
+import soot.jimple.ReturnVoidStmt;
+import soot.jimple.StaticInvokeExpr;
+import soot.jimple.Stmt;
+import soot.jimple.StringConstant;
+import soot.jimple.internal.JimpleLocal;
+import soot.jimple.internal.JimpleLocalBox;
+import soot.util.Chain;
+import ar.uba.dc.analysis.memory.code.NewStatement;
+import ar.uba.dc.analysis.memory.code.StatementVisitor;
+import ar.uba.dc.analysis.memory.code.impl.DefaultCallStatement;
+import ar.uba.dc.analysis.memory.code.impl.DefaultNewStatement;
+import ar.uba.dc.analysis.memory.code.impl.visitor.IdStatementVisitor;
+import ar.uba.dc.memory.analysis.compositional.EscapeAnalysisResult;
+import ar.uba.dc.memory.analysis.compositional.madeja.MadejaEscapeAnalysisResult;
+
+public class InstrumenterForJPF  extends BodyTransformer { 
+	
+		private static boolean countAfterReturn = false;
+		
+		private static Log log = LogFactory.getLog(InstrumenterForJPF.class);
+		
+		//public static final String LISTENER_PACKAGE = "ar.uba.dc.tools.instrumentation.resource.tracker.runtime";
+		public static final String LISTENER_PACKAGE = "monitor";
+		public  static final String LISTENER_CLASS =  LISTENER_PACKAGE + ".BooleanTracker";
+		
+		private static final String NEW_OBJECT_SIGNATURE = "void track()";
+		private static final String MAIN_METHOD_SIGNATURE = "void main(java.lang.String[])";
+		private static final String CALL_SIGNATURE = "void start()";
+		private static final String CAUGHT_EXCEPTION_SIGNATURE = "void caughtExceptionEvent(java.lang.String)";		
+		private static final String END_CALL_SIGNATURE = "void stop()";
+		private static final String ENTER_PROGRAM_METHOD_SIGNATURE = "void start()";
+		private static final String EXIT_PROGRAM_METHOD_SIGNATURE = "void stop()";
+		
+		private String currentNewStmt; 
+		
+		private String mainClass;
+
+		private long newCounter = 0;
+		
+		private long invokeCounter = 0;
+		
+		private LifeTimeSummaryRepository repository;
+		
+		private StatementVisitor<String> visitor = new IdStatementVisitor();
+		
+		@SuppressWarnings("unchecked")
+		private List endCallInstrumentation = null;
+		
+		public InstrumenterForJPF(String mainClass, LifeTimeSummaryRepository repository) {
+			this.mainClass = mainClass;
+			this.repository = repository;
+		}
+		
+		
+		private void reset() {
+			this.newCounter = 0;
+			this.invokeCounter = 0;
+			this.currentNewStmt = null;
+			if(this.endCallInstrumentation != null)
+				throw new RuntimeException("Unable to instrument endCall event for: " + this.endCallInstrumentation.toString());
+		}
+	
+		
+		
+		@SuppressWarnings("unchecked")
+		protected void internalTransform(Body body, String phaseName, Map options) {
+			
+			SootMethod sootMethod = body.getMethod();			
+			EscapeAnalysisResult escapeAnalysisResult = new MadejaEscapeAnalysisResult(sootMethod);
+
+			if(!sootMethod.getDeclaringClass().getPackageName().startsWith(LISTENER_PACKAGE) 
+					/*&& this.repository.contains(sootMethod)*/) {
+				Chain units = body.getUnits();
+				Iterator<Unit> stmtIt = units.snapshotIterator();
+				
+				//Iterator<Unit> nextStmtIt = units.snapshotIterator();
+				
+				boolean enterEventInserted = false;
+				
+				while (stmtIt.hasNext()) {
+					Stmt next = (Stmt) stmtIt.next();
+					
+					// Para insertar la llamada al evento de entrada debemos esperar a pasar las primeras asignaciones de tipo "var = @this" y "param1 = @param1"
+					if(!(next instanceof IdentityStmt) && !enterEventInserted) {
+						if (sootMethod.getSubSignature().equals(MAIN_METHOD_SIGNATURE) && sootMethod.getDeclaringClass().getName().equals(mainClass)) {
+							units.insertBefore(addMainEnterEvent(sootMethod.getSignature()), next);
+						} else {
+							units.insertBefore(addMethodEnterEvent(sootMethod.getSignature()), next);
+						}
+						enterEventInserted = true;
+					}
+					processStmt(body, units, next, escapeAnalysisResult);
+				}
+						
+			
+				this.reset();
+			} else {
+				log.info("Ignorando..." + body.getMethod().toString());
+			}
+		}
+
+		private  List<Unit> addMainEnterEvent(String signature) {
+			//FIXME
+			return this.newStaticInvokeExpr("void enter(java.lang.String)", StringConstant.v(signature));
+		}
+
+		/**
+		 * @param body
+		 * @param units
+		 * @param analisisVivas
+		 * @param cParametersAum
+		 * @param s
+		 * @param nextStmtIt 
+		 */
+		@SuppressWarnings("unchecked")
+		private void processStmt(Body body, Chain units, Stmt s, EscapeAnalysisResult result) {
+
+			SootMethod sootMethod = body.getMethod();
+			
+			//boolean proccessEndCallEvent = true;
+			
+			//NewStmt
+			if (s instanceof AssignStmt) {
+		
+				AssignStmt as = (AssignStmt) s;
+				Value exp = as.getRightOp();
+
+				// Si es un NEW se instrumenta como creation site
+				if (exp instanceof AnyNewExpr) {
+					JimpleLocal localVar = (JimpleLocal) as.getLeftOp();
+					units.insertBefore(this.addNewObjectEvent(),s);					
+				}
+				
+			}
+			
+			if (s instanceof IdentityStmt) {
+				IdentityStmt is = (IdentityStmt) s;
+				Value exp = is.getRightOp();
+				if (exp instanceof CaughtExceptionRef) {
+					units.insertAfter(this.addCaughtExceptionEvent(sootMethod.toString()), s);
+				}
+				
+			}
+			
+			// Para los calls instrumentamos el mapping de particiones
+			if (s.containsInvokeExpr()) {				
+				InvokeExpr expr = s.getInvokeExpr();
+				if(!expr.getMethod().getDeclaringClass().getName().contains(LISTENER_CLASS) ) {
+					DefaultCallStatement callStatement = new DefaultCallStatement(sootMethod, s, this.invokeCounter);
+
+					SootMethod method = expr.getMethod();
+					List before = null;
+					List after = null;
+					boolean isReturn = !method.getReturnType().equals(VoidType.v());
+					
+					if (method.getDeclaringClass().getName().equals("java.lang.System") && EXIT_PROGRAM_METHOD_SIGNATURE.equals(method.getSubSignature())) {
+						before = this.addMainMethodExitEvent(method.toString());
+					} else {
+						//before = this.addCallEvent(method, body, s, callStatement.apply(this.visitor));
+						//after = this.addEndCallEvent(method, body, s, callStatement.apply(this.visitor));
+						
+					}
+					
+					if (before != null) 
+						units.insertBefore(before, s);
+			
+										
+				} else {
+					log.info("Ignoring call to " + expr.getMethod().toString());
+				}
+				
+				//Actualizamos el counter
+				this.invokeCounter++;
+			} else if(s instanceof ReturnStmt  || s instanceof ReturnVoidStmt ) {
+				
+				if(this.endCallInstrumentation != null) {
+					//Hay pendiente un endCall de un metodo con return
+					units.insertBefore(this.endCallInstrumentation, s);
+					this.endCallInstrumentation = null;
+				}
+				
+				//Main => export result
+				if(sootMethod.equals(Scene.v().getMainClass().getMethod("void main(java.lang.String[])"))) {
+					units.insertBefore(addMainMethodExitEvent(sootMethod.getSignature()),s);	
+				} else {
+					units.insertBefore(addMethodExitEvent(sootMethod.getSignature()),s);	
+				}
+			}
+			
+			
+			
+		}
+
+		private List<Unit> addCallEvent(SootMethod method, Body body, Stmt s, String invokeStmt) {
+			List<Unit> code = new Vector<Unit>();
+			//addNewStaticInvokeExpr(code, CALL_SIGNATURE, StringConstant.v(invokeStmt));
+			addNewStaticInvokeExpr(code, CALL_SIGNATURE);
+			return code;
+		}
+		
+		private List<Unit> addEndCallEvent(SootMethod method, Body body, Stmt s, String invokeStmt) {
+			List<Unit> code = new Vector<Unit>();
+			// addNewStaticInvokeExpr(code, END_CALL_SIGNATURE, StringConstant.v(invokeStmt));
+			addNewStaticInvokeExpr(code, END_CALL_SIGNATURE);
+
+			return code;
+		}
+
+		/**
+		 * @param localVar 
+		 * @param insSite
+		 * @param partition
+		 * @return
+		 */
+		private List<Unit> addNewObjectEvent(JimpleLocal localVar, String insSite) {
+			List<Unit> code = new Vector<Unit>();
+	//		addNewStaticInvokeExpr(code, NEW_OBJECT_REGISTRATION_SIGNATURE, StringConstant.v(insSite), StringConstant.v(partition));
+	//		addNewStaticInvokeExpr(code, NEW_OBJECT_SIGNATURE, localVar, StringConstant.v(insSite));
+			addNewStaticInvokeExpr(code, NEW_OBJECT_SIGNATURE);
+			return code;
+		}
+		private List<Unit> addNewObjectEvent() {
+			List<Unit> code = new Vector<Unit>();
+			addNewStaticInvokeExpr(code, NEW_OBJECT_SIGNATURE);
+			return code;
+		}
+		
+		
+		@SuppressWarnings("unchecked")
+		private List addNewArrayEvent(JimpleLocal localVar, String insSite, Body body, Stmt s) {
+
+			AssignStmt as = (AssignStmt) s;
+			Value exp = as.getRightOp();
+			List newArrayParam = new Vector();
+			
+			if (exp instanceof AnyNewExpr) {
+				//1 object
+				if (exp instanceof NewExpr) {
+					newArrayParam.add(IntConstant.v(0));
+				} else if (exp instanceof NewArrayExpr) { //Array
+					NewArrayExpr newArrExp = (NewArrayExpr) exp;
+					newArrayParam.add(newArrExp.getSize());
+				} else if (exp instanceof NewMultiArrayExpr) { //MultiArray
+					NewMultiArrayExpr newMArrayExp = (NewMultiArrayExpr) exp;
+					for (Iterator iter = newMArrayExp.getSizes().iterator(); iter.hasNext();) {
+						Value element = (Value) iter.next();
+						newArrayParam.add(element);
+					}
+				}
+
+			}
+			
+			//Generamos el codigo
+			List<Unit> code = new Vector<Unit>();
+			//addNewStaticInvokeExpr(code, NEW_OBJECT_REGISTRATION_SIGNATURE, StringConstant.v(insSite), StringConstant.v(partId));
+			addNewStaticInvokeExpr(code, NEW_OBJECT_SIGNATURE, localVar, StringConstant.v(insSite));
+			
+			/*
+			String sMethodtoCall = "void newArrayEvent(java.lang.Object,java.lang.String";
+			for (int i = 0; i < newArrayParam.size(); i++)
+				sMethodtoCall += ",int";
+			sMethodtoCall += ")";
+			
+			SootMethod toCall = this.getTargetMethod(sMethodtoCall);
+
+			List args = new Vector();
+			args.add(localVar);
+			args.add(StringConstant.v(insSite));
+			//args.add(StringConstant.v(partId));
+			args.addAll(newArrayParam);
+			StaticInvokeExpr invExpr = Jimple.v().newStaticInvokeExpr(toCall.makeRef(), args);
+			Stmt invStmt = Jimple.v().newInvokeStmt(invExpr);
+			code.add(invStmt);
+			*/
+			return code;
+		}
+		
+		
+	private List<Unit> newStaticInvokeExpr(String methodDescription, Value... values) {
+		List<Unit> code = new Vector<Unit>();
+		SootMethod toCall = this.getTargetMethod(methodDescription);
+		StaticInvokeExpr invExpr = Jimple.v().newStaticInvokeExpr(toCall.makeRef(), Arrays.asList(values));
+		Stmt invStmt = Jimple.v().newInvokeStmt(invExpr);
+		code.add(invStmt);
+		return code;
+	}
+	
+	private void addNewStaticInvokeExpr(List<Unit> code , String methodDescription, Value... values) {
+		SootMethod toCall = this.getTargetMethod(methodDescription);
+		StaticInvokeExpr invExpr = Jimple.v().newStaticInvokeExpr(toCall.makeRef(), Arrays.asList(values));
+		Stmt invStmt = Jimple.v().newInvokeStmt(invExpr);
+		code.add(invStmt);
+	}
+	
+	private List<Unit> addCaughtExceptionEvent(String caughtInMethod) {
+		return this.newStaticInvokeExpr(CAUGHT_EXCEPTION_SIGNATURE, StringConstant.v(caughtInMethod));
+	}
+		
+	private SootMethod getTargetMethod(String methodDescription) {
+	//	Scene.v().addBasicClass(LISTENER_CLASS,2);
+	//	Scene.v().loadClassAndSupport(LISTENER_CLASS);
+		return Scene.v().getSootClass(LISTENER_CLASS).getMethod(methodDescription);
+	}
+	
+	@SuppressWarnings("unchecked")
+	private List addMethodEnterEvent(String method) {
+		return this.newStaticInvokeExpr("void enter(java.lang.String)", StringConstant.v(method));
+	}
+	
+	@SuppressWarnings("unchecked")
+	private List addMethodExitEvent(String method) {
+		return this.newStaticInvokeExpr("void exit(java.lang.String)", StringConstant.v(method));
+	}
+	
+
+	@SuppressWarnings("unchecked")
+	private List addMainMethodExitEvent(String method) {
+		List code = new Vector();
+		SootMethod toCall = this.getTargetMethod("void exit(java.lang.String)");
+		StaticInvokeExpr invExpr = Jimple.v().newStaticInvokeExpr(toCall.makeRef(),  StringConstant.v(method));
+		Stmt invStmt = Jimple.v().newInvokeStmt(invExpr);
+		code.add(invStmt);
+		toCall = this.getTargetMethod("void exit()");
+		invExpr = Jimple.v().newStaticInvokeExpr(toCall.makeRef());
+		invStmt = Jimple.v().newInvokeStmt(invExpr);
+		code.add(invStmt);
+		return code;
+		
+	}
+
+	
+}
+
