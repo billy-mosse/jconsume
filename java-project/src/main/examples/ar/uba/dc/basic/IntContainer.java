6903cd82604236ed660f92dbd068056fe0db6d79
Billy Mosse <billy.mosse@gmail.com>
2016-06-13 12:56:22 -0300

    Added some files for the SubstractWithFolds example

---
+++ java-project/src/main/examples/ar/uba/dc/basic/IntContainer.java
@@ -0,0 +1,11 @@
+package ar.uba.dc.basic;
+
+
+public class IntContainer
+{
+	public Integer apply(int n)
+	{
+		Integer i = new Integer(4);
+		return i;
+	}
+}
---
+++ java-project/src/main/examples/ar/uba/dc/basic/IntContainerSon.java
@@ -0,0 +1,12 @@
+package ar.uba.dc.basic;
+
+
+public class IntContainerSon extends IntContainer
+{
+	public Integer apply(int n)
+	{
+		//pero con otro invariante
+		Integer i = new Integer(8);
+		return i;
+	}
+}
