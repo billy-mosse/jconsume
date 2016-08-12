3db1606b3484fd14a733e357c2f0180b6ec109bd
Billy Mosse <billy.mosse@gmail.com>
2016-08-12 12:25:23 -0300

    now using soot 2.5.0

--- dependencies/install.sh
+++ dependencies/install.sh
@@ -7,7 +7,7 @@ mvn install:install-file -Dfile=madeja-1.0.jar -DgroupId=com.verimag -DartifactI
 
 #Soot
 mvn install:install-file -Dfile=sootlib/sootclasses.jar -DgroupId=ca.mcgill.sable -DartifactId=soot -Dversion=2.4.0 -Dpackaging=jar
-mvn install:install-file -Dfile=sootlib/jasminclasses-2.4.0.jar -DgroupId=ca.mcgill.sable -DartifactId=jasmin -Dversion=2.4.0 -Dpackaging=jar
+mvn install:install-file -Dfile=sootlib/jasminclasses-2.5.0.jar -DgroupId=ca.mcgill.sable -DartifactId=jasmin -Dversion=2.4.0 -Dpackaging=jar
 mvn install:install-file -Dfile=sootlib/polyglotclasses-1.3.5.jar -DgroupId=ca.mcgill.sable -DartifactId=polyglot -Dversion=1.3.5 -Dpackaging=jar
 
 
