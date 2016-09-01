19f455ac249fb21914168d4bed9dad1906d535d5
Billy Mosse <billy.mosse@gmail.com>
2016-09-01 13:01:57 -0300

    Paper memory analysis
                I'm commiting in several pieces because of a strange bug that happens while pushing

---
+++ java-project/src/main/java/ar/uba/dc/analysis/memory/PaperMemorySummaryFactory.java
@@ -0,0 +1,14 @@
+package ar.uba.dc.analysis.memory;
+
+import ar.uba.dc.analysis.common.intermediate_representation.IntermediateRepresentationMethod;
+import ar.uba.dc.analysis.memory.summary.MemorySummary;
+
+public class PaperMemorySummaryFactory {
+
+        public PaperMemorySummary initialSummary(IntermediateRepresentationMethod ir_method) {
+                PaperMemorySummary paperMemorySummary = null;
+
+                return paperMemorySummary;
+        }
+
+}
