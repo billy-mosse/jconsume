3bbafe9db2f163c056a00a932a919454c264589d
Billy Mosse <billy.mosse@gmail.com>
2016-10-04 11:18:50 -0300

    Started the bitransformation from barvinok expressions to invariants

---
+++ java-project/src/main/java/ar/uba/dc/barvinok/expression/operation/InvariantBuilder.java
@@ -0,0 +1,14 @@
+package ar.uba.dc.barvinok.expression.operation;
+
+import ar.uba.dc.barvinok.expression.DomainSet;
+
+public class InvariantBuilder {
+
+	public DomainSet generateFromExpression(String constraints)
+	{
+		DomainSet ret = null;
+		
+		
+		return ret;
+	}
+}
