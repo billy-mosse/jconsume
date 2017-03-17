2f867a9767db6cec6fd4e067b760a7c4a31a48ac
Billy Mosse <billy.mosse@gmail.com>
2017-03-17 14:21:44 -0300

    Program 2

--- java-project/src/main/examples/ar/uba/dc/paper/Program2Test.java
+++ java-project/src/main/examples/ar/uba/dc/paper/Program2Test.java
@@ -3,7 +3,7 @@ package ar.uba.dc.paper;
 public class Program2Test {
 	public static void main(String[] args) {
 		int count = Integer.parseInt(args[0]);
-		for(int i=1;i<=count;i++)
+		for(int i=0;i<=count;i++)
 		{
 			String[] argsTest=new String[1];
 			argsTest[0]=Integer.toString(i);
