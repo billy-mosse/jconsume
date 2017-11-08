0eec74e41dc7f5d0ff5dfefaefaf3c31988c648f
Billy Mosse <billy.mosse@gmail.com>
2017-11-08 13:53:39 -0300

    example for the relevant fields of non instrumented classes problem

--- java-project/src/main/examples/ar/uba/dc/daikon/A.java
+++ java-project/src/main/examples/ar/uba/dc/daikon/A.java
@@ -8,17 +8,38 @@ package ar.uba.dc.daikon;
 
 public class A {
 
-	public static int pi = 314;
 	public int a;
+	public int c;
 	private int b;
 	
 	public int getb()
 	{
-		return b;
+		return this.b;
 	}
 	
 	public void setb(int b)
 	{
 		this.b = b;
 	}
+	
+	public static void createNew(int i)
+	{
+		Integer k = new Integer(i);
+	}
+	
+	public void doSomeHeavyStuff()
+	{
+		for(int i = 0; i < 100; i++)
+		{
+			this.a = i;
+			for(int j = 0; j < 1100; j++)
+			{
+				this.b = this.a;
+				Integer k = new Integer(4);
+			}
+		}
+	}
+	
+	public static void doNothingForInstrmentedMethod()
+	{}
 }
--- java-project/src/main/examples/ar/uba/dc/daikon/Ins5.java
+++ java-project/src/main/examples/ar/uba/dc/daikon/Ins5.java
@@ -1,7 +1,5 @@
 package ar.uba.dc.daikon;
 
-import ar.uba.dc.util.ListC;
-import ar.uba.dc.util.ListItr;
 
 /**
  * Tiene un for que va hasta un field
@@ -12,17 +10,21 @@ public class Ins5 {
 	public static void main(String[] args) {
 		
 		int n = Integer.parseInt(args[0]);
-		InstrumentedMethod2.a4(n,args);
 		mainParameters(n);		
 	}
 	
 	public static void mainParameters(int n)
 	{
 		A cosa = new A();
+		cosa.setb(n);
+		//f(cosa.a);
 		for(cosa.a = 1; cosa.a < n; cosa.a++)
 		{
 			A otra_cosa = new A();
-			InstrumentedMethod2.a7(cosa,n);
 		}
+		
+		//System.out.print(cosa.getb());
+		//A.createNew(4);
 	}
+
 }
