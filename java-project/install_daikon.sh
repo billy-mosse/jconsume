c95b6289b138523836a5fa281c9150bdf592b42e
Guillermo Mosse <billy.mosse@gmail.com>
2018-06-04 16:00:51 -0300

    first version of install daikon script

---
+++ java-project/install_daikon.sh
@@ -0,0 +1,13 @@
+DAIKONPARENT=/home/billy/Downloads/daikontest
+
+wget http://plse.cs.washington.edu/daikon/download/daikon-5.6.4.tar.gz
+tar zxf daikon-5.6.4.tar.gz
+
+# The absolute pathname of the directory that contains Daikon
+export DAIKONDIR=$DAIKONPARENT/daikon-5.6.4
+# The absolute pathname of the directory that contains the Java JDK
+export JAVA_HOME=/usr/lib/jvm/java
+source $DAIKONDIR/scripts/daikon.bashrc
+export JAVA_HOME=${JAVA_HOME:-$(dirname $(dirname $(dirname $(readlink -f $(/usr/bin/which java)))))}
+
+
