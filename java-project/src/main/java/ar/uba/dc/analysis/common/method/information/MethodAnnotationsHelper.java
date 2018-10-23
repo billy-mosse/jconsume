bb7e868304f22128af1b5be925b630c0d126e9e4
Guillermo Mosse <billy.mosse@gmail.com>
2018-10-23 12:08:21 -0300

    bug fix for memory summary annotations

--- java-project/src/main/java/ar/uba/dc/analysis/common/method/information/MethodAnnotationsHelper.java
+++ java-project/src/main/java/ar/uba/dc/analysis/common/method/information/MethodAnnotationsHelper.java
@@ -5,8 +5,12 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import ar.uba.dc.analysis.common.intermediate_representation.IntermediateRepresentationMethod;
 import ar.uba.dc.analysis.common.method.information.rules.Rule;
+import ar.uba.dc.analysis.memory.PaperInterproceduralAnalysis;
 import ar.uba.dc.analysis.memory.expression.ParametricExpression;
 import ar.uba.dc.analysis.memory.impl.BarvinokParametricExpression;
 import ar.uba.dc.analysis.memory.impl.madeja.PaperMemorySummary;
@@ -16,7 +20,8 @@ import ar.uba.dc.barvinok.expression.QuasiPolynomial;
 //TODO cambiar el nombre
 public class MethodAnnotationsHelper {
 
-	
+	private static Log log = LogFactory.getLog(MethodAnnotationsHelper.class);
+
 	private String mainClass;
 	
 	//Por ahora voy a asumir que son memory summaries.
@@ -46,23 +51,31 @@ public class MethodAnnotationsHelper {
 	
 	public PaperMemorySummary get(IntermediateRepresentationMethod ir_method)
 	{
-		Annotation annotation = this.annotationsContainer.annotations.get(ir_method.toString());
-		
-		if(annotation == null)
-			return null;
-
-		//
-		QuasiPolynomial q = new QuasiPolynomial("0");
-		PiecewiseQuasipolynomial p = new PiecewiseQuasipolynomial();
-		p.add(q);
-		ParametricExpression temp = new BarvinokParametricExpression(p);
-		PaperMemorySummary summary = new PaperMemorySummary(ir_method, temp, new BarvinokParametricExpression(annotation.consumption));
-		
-		
-		
-		return summary;
-		
-		
+		Annotation annotation;
+		try
+		{
+			if(this.annotationsContainer != null)
+			{
+				annotation = this.annotationsContainer.annotations.get(ir_method.toString());
+			
+				if(annotation != null)
+				{
+					QuasiPolynomial q = new QuasiPolynomial("0");
+					PiecewiseQuasipolynomial p = new PiecewiseQuasipolynomial();
+					p.add(q);
+					ParametricExpression temp = new BarvinokParametricExpression(p);
+					PaperMemorySummary summary = new PaperMemorySummary(ir_method, temp, new BarvinokParametricExpression(annotation.consumption));
+					
+					return summary;
+				}
+			}
+		}
+		catch(java.lang.NullPointerException e )
+		{
+			log.error(e.getMessage(), e);
+			System.exit(1);
+		}
+		return null;
 		
 	}
 
