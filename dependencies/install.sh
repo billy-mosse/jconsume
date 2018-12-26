417046f0600e4d3e68f4393ce0b9bb4d67f354c3
Guillermo Mosse <billy.mosse@gmail.com>
2018-12-26 17:25:24 -0300

    External jars added to POM. mvn compile working again.

--- dependencies/install.sh
+++ dependencies/install.sh
@@ -3,13 +3,14 @@
 #Installing 3rd party JARs
 
 #Madeja
-mvn install:install-file -Dfile=madeja-1.0.jar -DgroupId=com.verimag -DartifactId=madeja -Dversion=1.0 -Dpackaging=jar
+mvn install:install-file -Dfile=madeja-1.0.jar -DgroupId=com.verimag -DartifactId=madeja -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true
 
 #Soot
-mvn install:install-file -Dfile=sootlib/sootclasses.jar -DgroupId=ca.mcgill.sable -DartifactId=soot -Dversion=2.4.0 -Dpackaging=jar
-mvn install:install-file -Dfile=sootlib/jasminclasses-2.5.0.jar -DgroupId=ca.mcgill.sable -DartifactId=jasmin -Dversion=2.4.0 -Dpackaging=jar
-mvn install:install-file -Dfile=sootlib/polyglotclasses-1.3.5.jar -DgroupId=ca.mcgill.sable -DartifactId=polyglot -Dversion=1.3.5 -Dpackaging=jar
+mvn install:install-file -Dfile=sootlib/sootclasses.jar -DgroupId=ca.mcgill.sable -DartifactId=soot -Dversion=2.4.0 -Dpackaging=jar -DgeneratePom=true
+mvn install:install-file -Dfile=soot-trunk.jar -DgroupId=ca.mcgill.sable -DartifactId=soot -Dversion=trunk -Dpackaging=jar -DgeneratePom=true
+mvn install:install-file -Dfile=sootlib/jasminclasses-2.5.0.jar -DgroupId=ca.mcgill.sable -DartifactId=jasmin -Dversion=2.4.0 -Dpackaging=jar -DgeneratePom=true
+mvn install:install-file -Dfile=sootlib/polyglotclasses-1.3.5.jar -DgroupId=ca.mcgill.sable -DartifactId=polyglot -Dversion=1.3.5 -Dpackaging=jar -DgeneratePom=true
 
 
 #jep
-mvn install:install-file -Dfile=jeplib/jep-2.4.1.jar -DgroupId=jep -DartifactId=jep -Dversion=2.4.1 -Dpackaging=jar
+mvn install:install-file -Dfile=jeplib/jep-2.4.1.jar -DgroupId=jep -DartifactId=jep -Dversion=2.4.1 -Dpackaging=jar -DgeneratePom=true
